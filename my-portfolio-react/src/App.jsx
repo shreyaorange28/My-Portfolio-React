6248d5035f5e62c16e1c8098bc0861f13c7b8849
Shreya Thothathri <shreyathoth@vt.edu>
2025-01-14 04:01:23 -0500

    Initialized React Project with Vite and Tailwind css

---
+++ my-portfolio-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    {/* min-h-screen: make*/}
+    <div className='min-h-screen w-full flex items-center justify-center bg-gradient-to-r from-purple-300 via-purple-200 to-purple-300'>
+      
+      </div>
+    </>
+  )
+}
