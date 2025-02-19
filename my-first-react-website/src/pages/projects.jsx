01e00f684750c94273439caa63243f99f021e3bc
Shreya Thothathri <shreyathoth@vt.edu>
2025-02-19 12:46:06 -0500

    Made the project title grow and shake
    Followed this tutorial - https://unused-css.com/blog/css-shake-animation/

--- my-first-react-website/src/pages/projects.jsx
+++ my-first-react-website/src/pages/projects.jsx
@@ -2,10 +2,9 @@ import "./projects.css";
 export function Projects() {
     return (
         <div className="page_body">
-            <h1>Projects</h1>
-
+            <h1 className="project_header">Projects</h1>
             <div className="project_content_block">
-                <h2 className="project_header">Project Name</h2>
+                <h2 className="project_title">Project Name</h2>
                 <p className="project_subtitle">Project Date and Location</p>
                 <p className ="project_content_body">blah blah blah description blah blah blah descriptionblah blah blah descriptionblah blah blah 
                     descriptionblah blah blah descriptionblah blah blah descriptionblah blah blah descriptionblah
