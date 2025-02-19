a81217a909dcad9e7fd64d0d000eb237ea7f081a
Shreya Thothathri <shreyathoth@vt.edu>
2025-02-19 12:33:39 -0500

    Adapted code from nav bar to make the project bar line change color when hovered on

--- my-first-react-website/src/pages/projects.jsx
+++ my-first-react-website/src/pages/projects.jsx
@@ -3,6 +3,7 @@ export function Projects() {
     return (
         <div className="page_body">
             <h1>Projects</h1>
+
             <div className="project_content_block">
                 <h2 className="project_header">Project Name</h2>
                 <p className="project_subtitle">Project Date and Location</p>
@@ -11,8 +12,8 @@ export function Projects() {
                      blah blah descriptionblah blah blah descriptionblah blah blah descriptionblah blah blah 
                      descriptionblah blah blah descriptionblah blah blah description</p>
                 <a href="" className="project_link"><p>Project Link</p></a>
-                
             </div>
+            
         </div>
     );
 }
