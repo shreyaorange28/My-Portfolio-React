e68aba7cba7066175dc77e338c6f0c8df84f8a2f
Shreya Thothathri <shreyathoth@vt.edu>
2025-04-15 23:01:30 -0400

    finished home page

--- my-first-react-website/src/pages/home.jsx
+++ my-first-react-website/src/pages/home.jsx
@@ -2,11 +2,22 @@ import "./home.css";
 export function Home() {
     return (
         <div className="page_body">
-            <span>
-                <h1 className="welcome">Welcome to my website!</h1>
-                <h2 className="intro">My name is Shreya and I am currently an undergraduate student at Virginia Tech</h2>
-                <h3 className="message">Feel free to look around (and maybe even hover over things...)</h3>
-            </span>
+          
+          <div className="content">
+            <h1 className="welcome">Welcome to my website! *</h1>
+            <h2 className="intro">
+              My name is Shreya and I am currently an undergraduate student at Virginia Tech
+            </h2>
+            <h3 className="message">
+              Feel free to look around (and maybe even hover over things...)
+            </h3>
+          </div>
+      
+          <div className="footnote">
+            <h3>* Insert really cool dynamic background here</h3>
+          </div>
+      
         </div>
-    );
+      );
+      
 }
