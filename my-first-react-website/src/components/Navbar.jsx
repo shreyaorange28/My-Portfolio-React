6b33b856cf7535cc25cff8d863ac7c4bdf892a10
Shreya Thothathri <shreyathoth@vt.edu>
2025-05-04 09:23:51 -0400

    Updated Navbar and Home page following mockup
    Used https://www.shapedivider.app/ to make hills

--- my-first-react-website/src/components/Navbar.jsx
+++ my-first-react-website/src/components/Navbar.jsx
@@ -7,8 +7,8 @@ export function Navbar() {
             <a href="/" className="logo">Shreya Thothathri&apos;s Website</a>
             <nav className="navbar">
                 <Link to="/" >Home</Link>
-                <Link to="/projects">Projects</Link>
                 <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </nav>
         </header>
